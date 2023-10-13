e1c58af5270c97a7f67c54d525d234f56e02eff5
Junior <junior.jr.jl@gmail.com>
2023-10-13 15:22:37 -0300

    feat: ajuste de atualização de lib

--- src/main/java/com/ead/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/ead/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,7 @@ package com.ead.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
